d527b7da15e53890051d4954982304a31055ce22
JoshCaravaggio <jdcaravaggio@gmail.com>
2020-06-16 22:41:53 -0400

    Changed seed file to fix bad password_digest

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,36 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 
-# ogGallery = Gallery.create(
-# :title => "Custom Neon Gallery",     
-# :description => "The Neon X custom neon gallery"
-# )
+Gallery.delete_all
+Image.delete_all
+Client.delete_all
 
-Client.create(:client_key => '0665151314', :password => '0665151314', :password_confirmation => '0665151314', :password_digest => '0665151314')
+ogGallery = Gallery.create(
+:title => "Custom Neon Gallery",     
+:description => "The Neon X custom neon gallery"
+)
 
-# File.open(Rails.root.join('db','images.txt'), "r") do |file_handle|
-#     counter = 0
-#     file_handle.each_line do |imageLine|    
+Client.create(:client_key => '0665151314', :password => '0665151314', :password_confirmation => '0665151314')
 
-#         splitString = imageLine.split("\\\"")
-#         imageSource = splitString[1]
-#         imageName = imageSource.split("files/")[2].split(".")[0]
+image_file = File.open(Rails.root.join('db','images.txt'), "r") 
+counter = 0
+image_file.each_line do |imageLine|    
+
+    splitString = imageLine.split("\\\"")
+    imageSource = splitString[1]
+    imageName = imageSource.split("files/")[2].split(".")[0]
+    
+    Image.create(
+        :source_url =>  imageSource, 
+        :metadata => '', 
+        :description => imageName, 
+        :index => counter, 
+        :gallery_id => ogGallery.id)   
         
-#         Image.create(
-#             :source_url =>  imageSource, 
-#             :metadata => '', 
-#             :description => imageName, 
-#             :index => counter, 
-#             :gallery_id => ogGallery.id)   
-            
-#         puts "Image created with name: #{imageName} and source: #{imageSource}"
-#         counter = counter + 1
+    puts "Image created with name: #{imageName} and source: #{imageSource}"
+    counter = counter + 1
 
-#     end
-# end
-
+end
+image_file.close
